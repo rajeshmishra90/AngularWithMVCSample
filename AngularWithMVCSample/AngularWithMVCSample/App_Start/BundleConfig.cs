84302b12878aa253430e21761d576c84bd7eb8ed
MishraRajesh <rajeshmishra90@gmail.com>
2016-07-01 14:38:52 +0530

    Working Sample

---
+++ AngularWithMVCSample/AngularWithMVCSample/App_Start/BundleConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Optimization;
+
+namespace AngularWithMVCSample
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/AwesomeAngularMVCApp")
+                    .IncludeDirectory("~/Scripts/Controllers", "*.js")
+                    .Include("~/Scripts/AngularWithMVCSample.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+        }
+    }
+}
---
+++ AngularWithMVCSample/AngularWithMVCSample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AngularWithMVCSample.MvcApplication" Language="C#" %>
