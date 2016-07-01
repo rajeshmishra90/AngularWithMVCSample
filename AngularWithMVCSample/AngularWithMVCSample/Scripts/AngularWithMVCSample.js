4803d0f9fb59664876b532f78d9cfd32d6418d7a
MishraRajesh <rajeshmishra90@gmail.com>
2016-07-01 23:05:01 +0530

    Route changes

--- AngularWithMVCSample/AngularWithMVCSample/Scripts/AngularWithMVCSample.js
+++ AngularWithMVCSample/AngularWithMVCSample/Scripts/AngularWithMVCSample.js
@@ -1,3 +1,32 @@
-﻿var AwesomeAngularMVCApp = angular.module('AwesomeAngularMVCApp', []);
+﻿var AwesomeAngularMVCApp = angular.module('AwesomeAngularMVCApp', ['ngRoute']);
 
 AwesomeAngularMVCApp.controller('LandingPageController', LandingPageController);
+
+var configFunction = function ($routeProvider) {
+    $routeProvider.
+        when('/routeOne', {
+            templateUrl: 'routesDemo/one'
+        })
+        .when('/routeTwo', {
+            templateUrl: 'routesDemo/two'
+        })
+        .when('/routeThree', {
+            templateUrl: 'routesDemo/three'
+        });
+}
+
+var configFunction = function ($routeProvider) {
+    $routeProvider.
+        when('/routeOne', {
+            templateUrl: 'routesDemo/one'
+        })
+        .when('/routeTwo/:donuts', {
+            templateUrl: function (params) { return '/routesDemo/two?donuts=' + params.donuts; }
+        })
+        .when('/routeThree', {
+            templateUrl: 'routesDemo/three'
+        });
+}
+configFunction.$inject = ['$routeProvider'];
+
+AwesomeAngularMVCApp.config(configFunction)
